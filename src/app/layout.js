a22ffc38566e94d711f77500cc8a92fb6a3f47b7
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2025-12-14 22:39:27 +0530

    Fully Ready

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import SocialMediaFloatingButton from "@/components/SocialMediaFloatingButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <SocialMediaFloatingButton />
       </body>
     </html>
   );
